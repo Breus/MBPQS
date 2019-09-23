8cf7848caf6f60b058ea53e68255c5e4c2cec0c4
Breus <b.blaauwendraad@gmail.com>
2019-09-23 14:19:28 +0200

    Tested multi-channel signing/verifying + growing

--- api_test.go
+++ api_test.go
@@ -1,4 +1,5 @@
 // Black box testing, that's why we have mbpqs_test as package instead of mbpqs.
+// This way, we know that accessibility of the api functions alone provides enough to fully function.
 package mbpqs_test
 
 import (
@@ -8,11 +9,17 @@ import (
 	"github.com/Breus/mbpqs"
 )
 
+// This test adds and verifies multiple channels,
+// consequently signs and verifies multiple messages in each channel,
+// grows the channel and signs/verifies new messages.
 func TestMultiChannels(t *testing.T) {
 	// Generate parameterized keypair.
-	var rootH uint32 = 2
-	var chanH uint32 = 10
-	sk, pk, err := mbpqs.GenKeyPair(32, rootH, chanH, 30, 4)
+	var rootH uint32 = 3
+	var chanH uint32 = 20
+	var ge uint32 = 20
+	var w uint16 = 4
+	var n uint32 = 32
+	sk, pk, err := mbpqs.GenKeyPair(n, rootH, chanH, ge, w)
 	if err != nil {
 		t.Fatalf("KeyGen failed: %s\n", err)
 	}
@@ -20,8 +27,9 @@ func TestMultiChannels(t *testing.T) {
 	// Add 2^rootH channels for testing.
 	for i := 0; i < (1 << rootH); i++ {
 		chIdx, rtSig, err := sk.AddChannel()
+		fmt.Printf("Added channel with ID: %d\n", chIdx)
 		if err != nil {
-			t.Fatalf("Adding %d-th channel gone wrong", i)
+			t.Fatalf("Adding %d-th channel failed with error %s\n", chIdx, err)
 		}
 		fmt.Printf("Created channel %d\n", chIdx)
 		acceptChannel, err := pk.VerifyChannel(rtSig)
@@ -34,16 +42,15 @@ func TestMultiChannels(t *testing.T) {
 
 		// Set the authnode to the root of the first chain tree.
 		authNode := rtSig.GetRootField()
-		fmt.Println(chanH)
 
-		// Now, we sign 2^chanH times, and verify the signatures.
+		// Now, we sign 2^chanH times, and verify the signatures in each channel.
 		for j := 0; j < int(chanH)-1; j++ {
 			msg := []byte("Message" + string(j))
-			sig, err := sk.SignChannelMsg(uint32(i+1), msg, false)
+			sig, err := sk.SignChannelMsg(chIdx, msg, false)
 			if err != nil {
-				t.Fatalf("Message signing in channel %d failed with error %s\n", i, err)
+				t.Fatalf("Message signing in channel %d failed with error %s\n", chIdx, err)
 			}
-			fmt.Printf("Signed message %d in channel %d\n", j, i)
+			fmt.Printf("Signed message %d in channel %d\n", j, chIdx)
 
 			acceptSig, err := pk.VerifyMsg(sig, msg, authNode)
 			if err != nil {
@@ -52,10 +59,44 @@ func TestMultiChannels(t *testing.T) {
 			if !acceptSig {
 				t.Fatalf("Verification of correct message/sig not accepted for message %d in channel %d\n", j, i)
 			} else {
-				fmt.Printf("Correctly verified message %d in channel %d\n", j, i)
+				fmt.Printf("Correctly verified message %d in channel %d\n", j, chIdx)
+			}
+			authNode = sig.GetAuthField()
+		}
+		// Let's grow the channels!
+		gs, err := sk.GrowChannel(chIdx)
+		if err != nil {
+			t.Fatalf("Growing channel %d failed with error %s\n", chIdx, err)
+		}
+		// Let's verifiy the growth signature.
+		acceptGrowth, err := pk.VerifyGrow(gs, authNode)
+		if err != nil {
+			t.Fatalf("Verification of growth channel %d failed with error: %s\n", chIdx, err)
+		}
+		if !acceptGrowth {
+			t.Fatalf("Correct growth of channel %d not accepted", chIdx)
+		}
+
+		authNode = gs.GetRootField()
+		// We have new keys to sign, lets use them!
+		for h := 0; h < int(chanH+ge-1); h++ {
+			msg := []byte("Message after growth" + string(h))
+			sig, err := sk.SignChannelMsg(chIdx, msg, false)
+			if err != nil {
+				t.Fatalf("Message signing in channel %d failed with error %s\n", chIdx, err)
+			}
+			fmt.Printf("Signed message %d in channel %d\n", h, chIdx)
+
+			acceptSig, err := pk.VerifyMsg(sig, msg, authNode)
+			if err != nil {
+				t.Fatalf("Verification message %d in channel %d failed with error %s\n", h, i, err)
+			}
+			if !acceptSig {
+				t.Fatalf("Verification of correct message/sig not accepted for message %d in channel %d\n", h, i)
+			} else {
+				fmt.Printf("Correctly verified message %d in channel %d\n", h, chIdx)
 			}
 			authNode = sig.GetAuthField()
 		}
 	}
-
 }
